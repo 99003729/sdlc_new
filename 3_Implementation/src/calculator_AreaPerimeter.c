cf1e58ae6a260f3c2af1a03f8afe4508b88b3eef
99003732 <78848729+99003732@users.noreply.github.com>
2021-03-14 10:54:52 +0530

    Update calculator_AreaPerimeter.c

--- 3_Implementation/src/calculator_AreaPerimeter.c
+++ 3_Implementation/src/calculator_AreaPerimeter.c
@@ -2,28 +2,56 @@
 
 double Area_Circle(double radius)
 {
-    double area_of_circle=0;
-    area_of_circle=PI*(radius*radius);
-    return area_of_circle;
+    if(radius>0)
+    {
+        double area_of_circle=0;
+        area_of_circle=PI*(radius*radius);
+        return area_of_circle;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 double Area_Square(double side)
 {
-    double area_of_square=0;
-    area_of_square=side*side;
-    return area_of_square;
+    if(side>0)
+    {
+        double area_of_square=0;
+        area_of_square=side*side;
+        return area_of_square;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 double Perimeter_Square(double side)
 {
-    double perimeter_of_square=0;
-    perimeter_of_square=side*4;
-    return perimeter_of_square;
+    if(side>0)
+    {
+        double perimeter_of_square=0;
+        perimeter_of_square=side*4;
+        return perimeter_of_square;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 double Perimeter_Rectangle(double l,double w)
 {
-    double perimeter_of_rectangle=0;
-    perimeter_of_rectangle=2*(l+w);
-    return perimeter_of_rectangle;
+    if(l>0 && w>0)
+    {
+        double perimeter_of_rectangle=0;
+        perimeter_of_rectangle=2*(l+w);
+        return perimeter_of_rectangle;
+    }
+    else
+    {
+        return 0;
+    }
 }
